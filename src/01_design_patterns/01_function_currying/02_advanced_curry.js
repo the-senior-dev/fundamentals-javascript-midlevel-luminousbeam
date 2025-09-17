1da99bfd50ec28d1e4850093ce8303132dc8845c
Jocelyn Jeriah <jocelyn.jeriah@gmail.com>
2025-09-17 15:10:12 -0400

    adding advanced currying solution

--- src/01_design_patterns/01_function_currying/02_advanced_curry.js
+++ src/01_design_patterns/01_function_currying/02_advanced_curry.js
@@ -1,6 +1,6 @@
 /* Problem 2: Implement the Curry Function
-Write a function named curry that takes a function fn as an argument. 
-The curry function should return a new function that can take multiple arguments 
+Write a function named curry that takes a function fn as an argument.
+The curry function should return a new function that can take multiple arguments
 either all at once or one at a time. For example:
     function add(a, b, c) {
         return a + b + c;
@@ -13,17 +13,16 @@ either all at once or one at a time. For example:
 
 // HINT: You will need to use recursion to solve this problem!
 function curry(fn) {
-    // Your code here
+    return function curried(...args) {
+        if (args.length >= fn.length) {
+            return fn(...args);
+        }
+        return function(...args2) {
+            return curried(...args, ...args2);
+        };
+    };
 }
-  
 
-// Example
-/*
-const add = (a, b, c) => a + b + c;
-const curriedAdd = curry(add);
-console.log(curriedAdd(1)(2)(3)); // Output should be 6
-console.log(curriedAdd(1, 2, 3)); // Output should also be 6
-*/
 
 module.exports = curry;
 
